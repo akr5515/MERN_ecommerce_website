e4334f2b3aa28a0ed0056de26c405e2c41e5edb5
akr5515 <akrangkon5515@gmail.com>
2020-12-19 19:47:40 +0600

    configuring cart for products

--- MERN_back_end/src/index.server.js
+++ MERN_back_end/src/index.server.js
@@ -9,6 +9,7 @@ const authRoutes = require("./routes/auth");
 const adminRoutes = require("./routes/admin/auth");
 const categoryRoutes = require("./routes/category");
 const productRoutes = require("./routes/product");
+const cartRoutes = require("./routes/cart");
 
 //environment variable or can say constants
 env.config();
@@ -35,6 +36,7 @@ app.use("/api", authRoutes);
 app.use("/api", adminRoutes);
 app.use("/api", categoryRoutes);
 app.use("/api", productRoutes);
+app.use("/api", cartRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running at port ${process.env.PORT}`);
